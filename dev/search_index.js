f877a6573efcfa1b2831d3eb5a28d392d5dbdffe
Documenter.jl <documenter@juliadocs.github.io>
2022-06-21 02:05:40 +0000

    build based on 2b9fc55

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NetRC","category":"page"},{"location":"#NetRC","page":"Home","title":"NetRC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NetRC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NetRC]","category":"page"}]
+}
